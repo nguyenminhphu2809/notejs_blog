b4e2b27ead3db4ba0c9f1f16df10c80906b2a5ba
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-12 21:31:01 +0700

    Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ 
@@ -24,24 +26,8 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 console.log(__dirname);
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  console.log(req.query.q);
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  // console.log(req.query.q);
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send("")
-})
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+const newRouter = require('./news');
+const siteRouter = require('./site')
+
+function route(app){
+    //code cũ
+    // app.get('/', (req, res) => {
+    //     res.render('home');
+    // });
+
+    // app.get('/news', (req, res) => {
+    // console.log(req.query.q);
+    // res.render('news');
+    // })
+    app.use('/news', newRouter);
+    app.use('/', siteRouter);
+
+    // app.get('/search', (req, res) => {
+    // // console.log(req.query.q);
+    //     res.render('search');
+    // });
+
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     res.send("");
+    // });
+}
+
+module.exports = route;
