6d36fa31d18bcde4dca1c496eb4434243d52a6f3
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-12 20:26:40 +0700

    Post method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ 
+  extended: true
+}));
+app.use(express.json());
+
 //HTTP Logger 
 // app.use(morgan('combined'));  
 
@@ -31,6 +36,11 @@ app.get('/news', (req, res) => {
 app.get('/search', (req, res) => {
   // console.log(req.query.q);
   res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send("")
 })
 
 app.listen(port, () => {
