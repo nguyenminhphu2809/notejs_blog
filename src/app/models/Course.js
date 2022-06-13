11770b463719b39ce9d368abcc4458317b8eaf6c
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-13 16:15:24 +0700

    [MVC] Model

---
+++ src/app/models/Course.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const Course = new Schema({
+    name: {type: 'string', maxlength: 255},
+    description: {type: 'string', maxlength: 655},
+    image: {type: 'string', maxlength: 255},
+    createdAt: {type: Date, default:Date.now},
+    createdAt: {type: Date, default:Date.now},
+  });
+
+  module.exports = mongoose.model('Course', Course);
---
+++ src/config/db/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { use } = require('../../routes/site');
+
+async function connect() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/f8_education_dev',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connect successfully!!!");
+    }catch(err) {
+        console.log("Connect failure!!!");
+    }
+}
+
+module.exports = {connect};
