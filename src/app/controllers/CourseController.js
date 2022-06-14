faf88d9d473b4cc3971ff65978c540216f61d5be
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-14 19:00:29 +0700

    [CRUD] Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,17 +1,30 @@
 const Course = require('../models/Course');
 const { mongooseToObject } = require('../../util/mongoose');
 
-
 class Courseontroller {
-
     //[GET] /courses/:slug
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
-            .then(course =>{
-                res.render('course/show', { 
-                    course: mongooseToObject(course)
+            .then((course) => {
+                res.render('courses/show', {
+                    course: mongooseToObject(course),
                 });
             })
+            .catch(next);
+    }
+
+    //[GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    //[POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCnKLvUrsBZW203awokL4gnj1qjLQ`
+        const course = new Course(formData);
+        course.save()
+            .then(() => res.redirect('/'))
             .catch(next);
     }
 }
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+
+mongoose.plugin(slug);
 
 const Course = new Schema({
     name: { type: 'string', maxlength: 255 },
     description: { type: 'string', maxlength: 655 },
     image: { type: 'string', maxlength: 255 },
-    slug: { type: 'string', maxlength: 255},
-    createdAt: { type: Date, default: Date.now },
-    createdAt: { type: Date, default: Date.now },
+    slug: { type: 'string', maxlength: 255 , slug: 'name', unique: true },
+    videoId: { type: 'string', maxlength: 255 },
+}, {
+    timestamp: true,
 });
 
 module.exports = mongoose.model('Course', Course);
