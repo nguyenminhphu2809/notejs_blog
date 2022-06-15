fc16c6dee623e153fc2de4c2afafd379e31f6b3f
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-15 14:00:05 +0700

    [CRUD] Delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -46,6 +46,13 @@ class Courseontroller {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
+
+    //[DELETE] /courses/:id
+    destroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
 }
 
 module.exports = new Courseontroller();
